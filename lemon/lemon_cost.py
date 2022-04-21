be86e29d6ea3f45fb81b8ecacbefa3fbae96825f
bill.2278 <bill.2278@kakaoenterprise.com>
2022-04-21 18:58:34 +0900

    update

---
+++ lemon/lemon_cost.py
@@ -0,0 +1,9 @@
+def print_lemon_cost(a: int):
+    if a<0:
+        print(0)
+    elif a<10:
+        print(10)
+    elif a<100:
+        print(99)
+    else:
+        print(100)
---
+++ lemon_test.py
@@ -0,0 +1,11 @@
+from lemon import lemon_cost, lemon_name
+
+lemon_name.print_lemon_name(-1)
+lemon_name.print_lemon_name(5)
+lemon_name.print_lemon_name(20)
+lemon_name.print_lemon_name(100)
+
+lemon_cost.print_lemon_cost(-1)
+lemon_cost.print_lemon_cost(5)
+lemon_cost.print_lemon_cost(20)
+lemon_cost.print_lemon_cost(100)
